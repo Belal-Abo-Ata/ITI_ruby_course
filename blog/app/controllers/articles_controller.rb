40ff076ed59f4beb1bd88d4df160df7f1066b715
Belal Abo Ata <belalbelalmohamed1234@gmail.com>
2024-05-18 16:14:39 +0300

    feat: solve lab 4

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy, :create, :new]
   def index
     @articles = Article.all
   end
@@ -14,8 +14,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
-    if @article.save
+    if can?(:create, @article) and @article.save
       redirect_to(@article)
     else
       render(:new, status: :unprocessable_entity)
@@ -28,21 +27,32 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    if @article.update(article_params)
+    if can?(:update, @article) and @article.update(article_params)
       redirect_to(@article)
     else
       render(:edit, status: :unprocessable_entity)
     end
   end
 
-  def destory
+  def destroy
     @article = Article.find(params[:id])
+    if can?(:destroy, @article)
+      @article.destroy
+    end
+
     redirect_to(root_path, status: :see_other)
+  end
+
+  def increase_report_count
+    @article = Article.find(params[:id])
+    @article.increment!(:report_count)
+    @article.save
+    redirect_to(@article)
   end
 
   private
   def article_params
-    params.require(:article).permit(:title, :body, :status)
+    params.require(:article).permit(:title, :body, :status, :image, :user_id)
   end
 
 end
